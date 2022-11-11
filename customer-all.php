e5cb2a8c89749f66824976a9b656765e62be755b
Munnahasan4 <73132674+freelancermunna4@users.noreply.github.com>
2022-11-11 23:57:19 +0600

    Update customer-all.php

--- customer-all.php
+++ customer-all.php
@@ -26,7 +26,7 @@ if(isset($_POST['submit'])){
 
 ?>
 <!-- Main Content -->
-<main class="main_content">
+<main class="main_content"> 
     <!-- Side Navbar Links -->
     <?php include("common/sidebar.php");?>
     <!-- Side Navbar Links -->
