6cbc5dccec7666f5abee4248de55a5e0c7d1c87e
Munna4 <73132674+freelancermunna4@users.noreply.github.com>
2023-03-13 07:36:16 -0700

    Update customer-edit.php

--- customer-edit.php
+++ customer-edit.php
@@ -25,7 +25,7 @@ if(isset($_POST['submit'])){
   }else{
   $sql = "UPDATE customer SET name='$name', email='$email', phone='$phone', address='$address',city='$city', file='$file_name', time='$time' WHERE id='$id'";
   }
-  
+   
   $query = mysqli_query($conn,$sql);
   if($query){ 
   $msg = "Successfully Updated Customer!";
